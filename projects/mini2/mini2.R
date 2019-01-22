d01bfed284ac6ef305ab14dd120b181024037f25
a290074 <tobias.tiger@consultant.volvo.com>
2019-01-22 11:02:09 +0100

    Mini2 put on hold for now, need more knowledge in R

--- projects/mini2/mini2.R
+++ projects/mini2/mini2.R
@@ -10,13 +10,15 @@ housedata <- read.csv("kc_house_data.csv", header = TRUE)
 
 ii <- sample(seq(1,dim(housedata)[1]),500)  # obtain a random sample of 500 indices
 mydata <- housedata[ii,]                    # select a random subset of your full dataset
-mydata <- subset(mydata, select = -c(waterfront, view, condition, grade))
+mydata <- subset(mydata, select = -c(id, date, waterfront, view, condition, grade))
 row.names(mydata) <- NULL            # assign new IDs to each row
-
-plot(log10(price)~log10(sqft_living), data = mydata)
-
-plot(mydata)
 
 mydata %>%
   gather(-price, key = "var", value = "value") %>%
   ggplot(aes(x=value, y=price)) + geom_point() + facet_wrap(~ var, scales = "free") + theme_bw()
+
+plot(log10(price)~log10(sqft_living), data = mydata)
+plot(log10(price)~log10(sqft_lot), data = mydata)
+plot(log10(price)~log10(sqft_basement), data = mydata)
+plot(log10(price)~log10(lat), data = mydata)
+
